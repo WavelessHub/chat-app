ffc630b0f2f5c623881ca7760aa35ac509fc1be0
Waveless <itzwaveless@gmail.com>
2023-05-06 17:35:53 +0530

    App Edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <main className="h-screen w-screen bg-[#a7bcff] flex items-center justify-center">
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/app">App</Link>
+
       <Outlet />
     </main>
   );
